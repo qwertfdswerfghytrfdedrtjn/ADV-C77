51542ccedb9105d1a38a1a08272e3013591d40a9
SAIRAM.S.K <110996688+qwertfdswerfghytrfdedrtjn@users.noreply.github.com>
2022-10-07 17:02:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var first_name ="SAIRAM";
+var second_name = "SK";
+
+console.log("MY CONSOLE LOG PROJECT");
+console.log("SAIRAM SK");
+
+console.log("12+13=");
+console.log(12+13);
+
+console.log("30-15=");
+console.log(30-15);
+
+console.log("28÷8=");
+console.log(28/8);
+
+console.log("12² =");
+console.log(Math.pow(12,2));
+
+
+
+
+
